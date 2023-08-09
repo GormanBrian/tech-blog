1b83eeded3b9a650fd0f07b6bb7ef4995a5db386
Brian Gorman <briangorman99@gmail.com>
2023-08-09 14:04:49 -0400

    add controllers folder with routes structure, use routes in server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const path = require("path");
 const express = require("express");
 const hbs = require("express-handlebars").create({ helpers });
 
+const routes = require("./controllers");
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -10,6 +12,8 @@ app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
 
 app.listen(PORT, () =>
   console.log(`Now listening at http://localhost:${PORT}`)
