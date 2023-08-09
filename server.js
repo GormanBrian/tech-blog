18e56c1686a4b333055cc77113ae2aaf458c9418
Brian Gorman <briangorman99@gmail.com>
2023-08-09 13:27:54 -0400

    add server.js, setup handlebars, setup express server

---
+++ server.js
@@ -0,0 +1,16 @@
+const path = require("path");
+const express = require("express");
+const hbs = require("express-handlebars").create({ helpers });
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(PORT, () =>
+  console.log(`Now listening at http://localhost:${PORT}`)
+);
