77b8752d6abca665d09ef3a7545b8188d7df68af
Brian Gorman <briangorman99@gmail.com>
2023-08-09 13:47:34 -0400

    add BlogPost model

---
+++ models/BlogPost.js
@@ -0,0 +1,38 @@
+const { Model, DataTypes } = require("sequelize");
+
+const sequelize = require("../config/connection");
+
+class BlogPost extends Model {}
+
+BlogPost.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    contents: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    author_name: {
+      type: DataTypes.STRING,
+      references: {
+        model: "user",
+        key: "name",
+      },
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    modelName: "blog_post",
+  }
+);
+
+module.exports = Product;
